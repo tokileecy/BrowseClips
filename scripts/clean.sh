43e14b30898ceedda917d5c8992340ed9085277f
tokileecy <tokileecy@gmail.com>
2022-07-27 10:37:25 +0000

    chore: remove scripts

--- scripts/clean.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-find . | grep /node_modules$ | grep -v /node_modules/ | xargs rm -fR
-rm -rf ./apps/strapi-app/.cache
-rm -rf ./apps/strapi-app/build
--- scripts/install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-bash -c "cd ./apps/next-app && yarn install"
-bash -c "cd ./apps/strapi-app && yarn install"
