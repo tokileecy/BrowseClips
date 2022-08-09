2611ccd4a7129dccf993b639bd350abff55c19fc
tokileecy <tokileecy@gmail.com>
2022-08-09 07:34:22 +0000

    feat(nest): addVideos larger then 50

--- apps/nest-app/src/videos/videos.service.ts
+++ apps/nest-app/src/videos/videos.service.ts
@@ -14,35 +14,48 @@ export class VideosService {
 
   async addVideoById(id: string[]) {
     const service = google.youtube('v3');
-    const res = await service.videos.list({
-      auth: GOOGLE_API_KEY,
-      part: [
-        'id',
-        'status',
-        'contentDetails',
-        'liveStreamingDetails',
-        'snippet',
-        'statistics',
-      ],
-      id,
-    });
+    const videoDatas = [];
 
-    const data = res.data.items.map((item) => ({
-      id: item.id,
-      channelId: item.snippet.channelId,
-      title: item.snippet.title,
-      description: item.snippet.description,
-      publishedAt: item.snippet.publishedAt,
-      thumbnails: item.snippet.thumbnails as any,
-      tags: item.snippet.tags as string[],
-      status: item.status as any,
-      statistics: item.statistics as any,
-      liveStreamingDetails: item.liveStreamingDetails as any,
-      contentDetails: item.contentDetails as any,
-    }));
+    while (id.length > 0) {
+      const ids = id.splice(0, 50);
+      const res = await service.videos.list({
+        auth: GOOGLE_API_KEY,
+        part: [
+          'id',
+          'status',
+          'contentDetails',
+          'liveStreamingDetails',
+          'snippet',
+          'statistics',
+        ],
+        id: ids,
+      });
 
-    return this.prisma.video.createMany({
-      data,
-    });
+      videoDatas.push(
+        ...res.data.items.map((item) => ({
+          id: item.id,
+          channelId: item.snippet.channelId,
+          title: item.snippet.title,
+          description: item.snippet.description,
+          publishedAt: item.snippet.publishedAt,
+          thumbnails: item.snippet.thumbnails as any,
+          tags: item.snippet.tags as string[],
+          status: item.status as any,
+          statistics: item.statistics as any,
+          liveStreamingDetails: item.liveStreamingDetails as any,
+          contentDetails: item.contentDetails as any,
+        })),
+      );
+    }
+
+    return this.prisma.$transaction(
+      videoDatas.map((data) =>
+        this.prisma.video.upsert({
+          where: { id: data.id },
+          update: { ...data },
+          create: { ...data },
+        }),
+      ),
+    );
   }
 }
