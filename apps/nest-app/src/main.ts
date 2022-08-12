f4854af44c969af77fdb1e71c18bb6129b98c19e
tokileecy <tokileecy@gmail.com>
2022-08-12 10:02:43 +0000

    feat(nest): upgrade nest, add repl and pid file

--- apps/nest-app/src/main.ts
+++ apps/nest-app/src/main.ts
@@ -1,11 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { writeFileSync } from 'fs';
+import * as path from 'path';
 import { AppModule } from './app.module';
+import { repl } from '@nestjs/core';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  if (process.env.NEST_REPL === '1') {
+    await repl(AppModule);
+  }
+
   if (process.env.APP_ENV === 'development') {
     app.enableCors();
   }
   await app.listen(4000);
 }
 bootstrap();
+writeFileSync(path.resolve(__dirname, '../tmp/pid'), process.pid.toString());
