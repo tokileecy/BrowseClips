8da1be0692e6ae30af238aab8b3041dc50daabdc
tokileecy <tokileecy@gmail.com>
2022-08-16 02:35:33 +0000

    feat(crawler): add local api lib

--- apps/crawler-app/src/api/index.ts
+++ apps/crawler-app/src/api/index.ts
@@ -1,77 +1,8 @@
-import apiInstance from './api-instance';
+import { Api } from '@vtuber_clip/api';
 
-class Api {
-  /**
-   *
-   * @returns { import('axios').AxiosResponse<{
-   *  id: string,
-   *  title: string?,
-   *  description: string?,
-   *  country: string?,
-   *  publishedAt: DateTime,
-   *  thumbnails: {
-   *    height: number,
-   *    width: number,
-   *    url: string,
-   *  }[],
-   * }> }
-   */
-  listChannels = () => {
-    return apiInstance.get<
-      {
-        id: string;
-        title?: string;
-        description?: string;
-        country?: string;
-        publishedAt: string;
-        thumbnails: {
-          height: number;
-          width: number;
-          url: string;
-        }[];
-      }[]
-    >('/channels');
-  };
+const NEST_URL = process.env.NEST_URL;
+const uri = new URL('', NEST_URL).href;
 
-  /**
-   *
-   * @param { string[] } ids
-   * @returns
-   */
-  addChannelByIds = (ids: string[]) => {
-    return apiInstance.post('/channels', {
-      ids,
-    });
-  };
-
-  // syncChannelVideos = () => {
-  //   return apiInstance.get('/channels/sync');
-  // };
-
-  /**
-   *
-   * @returns { import('axios').AxiosResponse<{
-   *  id: string,
-   *  title: string?,
-   *  description: string?,
-   *  country: string?,
-   *  publishedAt: DateTime,
-   *  thumbnails: {
-   *    height: number,
-   *    width: number,
-   *    url: string,
-   *  }[],
-   * }> }
-   */
-  listVideos = () => {
-    return apiInstance.get('/videos');
-  };
-
-  addVideoByIds = (ids: string[]) => {
-    return apiInstance.post('/videos', {
-      ids,
-    });
-  };
-}
-
-export default new Api();
+export default new Api({
+  baseURL: uri,
+});
