c1b7a21697867abd65b5af7205108a846051b233
tokileecy <tokileecy@gmail.com>
2022-08-17 18:56:02 +0000

    feat(nest): adjust logger middleware

--- apps/nest-app/src/common/middleware/logger.middleware.ts
+++ apps/nest-app/src/common/middleware/logger.middleware.ts
@@ -1,15 +1,27 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
+  private readonly logger = new Logger('HTTP');
+
   use(req: Request, res: Response, next: NextFunction) {
-    const { method, url, ip } = req;
-    console.log(
-      `[Nest] ${
-        process.pid
-      }  - ${new Date().toISOString()}    ${method} ${url} - ${ip}`,
-    );
+    res.on('finish', () => {
+      const { method, originalUrl, ip } = req;
+      const { statusCode, statusMessage } = res;
+
+      const message = `${method} ${originalUrl} ${statusCode} ${statusMessage} FROM ${ip}`;
+
+      if (statusCode >= 500) {
+        return this.logger.error(message);
+      }
+
+      if (statusCode >= 400) {
+        return this.logger.warn(message);
+      }
+
+      return this.logger.log(message);
+    });
     next();
   }
 }
