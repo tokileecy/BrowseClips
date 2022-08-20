aa54893fcd2063829a4218a3358d1cd820092815
tokileecy <tokileecy@gmail.com>
2022-08-20 18:42:29 +0000

    fix(crawler): failed handle

--- apps/crawler-app/src/ioCommends.ts
+++ apps/crawler-app/src/ioCommends.ts
@@ -4,19 +4,20 @@ import api from './api';
 
 export default (io: Server, socket: Socket) => {
   socket.on('crawl-videos', async () => {
-    const channelIds = (await api.listChannels()).data.map(
-      (channel) => channel.id,
-    );
-
-    const videoIds = await listVideoIdsByChannelIds(channelIds);
-
     try {
+      const channelIds = (await api.listChannels()).data.map(
+        (channel) => channel.id,
+      );
+
+      const videoIds = await listVideoIdsByChannelIds(channelIds);
+
       console.log('add videos');
       api.addVideoByIds(
         Object.values(videoIds).reduce((acc, curr) => [...acc, ...curr], []),
       );
     } catch (error) {
       console.error(`addVideos failed`);
+      console.error(error);
     }
   });
 };
