f3cf6f1a77edc4ef651c3546b15f29c1c373460d
tokileecy <tokileecy@gmail.com>
2022-08-20 11:35:34 +0000

    fix(nest): add channel failed

--- apps/nest-app/src/channels/dto/create-channel.dto.ts
+++ apps/nest-app/src/channels/dto/create-channel.dto.ts
@@ -1,8 +1,6 @@
-import { IsNumberString } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateTodoDto {
-  @IsNumberString({ each: true })
   @ApiProperty()
   public readonly ids: string[];
 }
